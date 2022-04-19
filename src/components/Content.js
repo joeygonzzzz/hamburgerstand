18e26174f08104eb0a29c1f62d60ae8da4096377
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-04-18 21:17:28 -0500

    center change

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,7 +8,7 @@ const Content = () => {
     <>
         <div className="menu-card">
             <img  src={ImageOne} alt="burger" className="h-full rounded mb-20 shadow" />
-            <div className="flex flex-col justify-center items-center">
+            <div className="center-content">
                 <h2 className="text-2xl mb-2">Double Meat Bacon Burger</h2>
                 <p className="mb-2">One of our signature items!</p>
                 <span>$10 combo w/fries and medium drink</span>
@@ -16,7 +16,7 @@ const Content = () => {
         </div>
         <div className="menu-card">
             <img  src={ImageTwo} alt="fajita" className="h-full rounded mb-20 shadow" />
-            <div className="flex flex-col justify-center items-center">
+            <div className="center-content">
                 <h2 className="text-2xl mb-2">Fajita Taco with Guacamole!</h2>
                 <p className="mb-2">Voted Best Fajita Taco In Frio County</p>
                 <span>$3.50 w/Guacamole, $3.00 plain</span>
@@ -24,7 +24,7 @@ const Content = () => {
         </div>
         <div className="menu-card">
             <img  src={ImageThree} alt="taco" className="h-full rounded mb-20 shadow" />
-            <div className="flex flex-col justify-center items-center">
+            <div className="center-content">
                 <h2 className="text-2xl mb-2">Potato, Egg and Cheese Taco!</h2>
                 <p className="mb-2">One of our many breakfast tacos</p>
                 <span>$2.50 w/Cheese, $2.00 Plain</span>

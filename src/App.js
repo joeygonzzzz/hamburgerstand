9058d4c9737832aaddfbc412d6b20165319e3d2a
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-04-19 22:23:11 -0500

    layout finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -7,11 +7,36 @@ import {Routes, Route} from 'react-router-dom';
 import About from './pages/about';
 import Menu from './pages/menu';
 import Contact from './pages/contact';
+import Dropdown from './components/Dropdown';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  }
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if(window.innerWidth > 768 && isOpen) {
+        setIsOpen(false)
+        console.log('is resized')
+      }
+    }
+
+    window.addEventListener('resize', hideMenu)
+
+    return () => {
+      window.removeEventListener('resize', hideMenu)
+    };
+  });
+
+
+
   return (
     <>
-      <Navbar />
+      <Navbar toggle={toggle}/>
+      <Dropdown isOpen={isOpen} toggle={toggle}/>
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route path="/menu" element={<Menu/>}/>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
         <h1 className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14">
             Rosa's Hamburger Stand
         </h1>
-        <Link to="/" className='py-6 px-10 bg-yellow-500 rounded-full text-3xl hover:bg-yellow-300 transition duration-300 ease-in-out flex items-center animate-bounce'>Order Now
+        <Link to="/" className='py-6 px-10 bg-yellow-500 rounded-full text-3xl hover:bg-yellow-300 transition duration-300 ease-in-out flex items-center animate-bounce' path='/'>Order Now
             <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6 ml-4"

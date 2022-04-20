8e9432edef6bb7606757f7a196c4fc6c58eaae14
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-04-19 21:29:04 -0500

    routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 
 import './App.css';
-import Hero from './components/Hero';
 import Navbar from './components/Navbar';
-import Content from './components/Content'
+import Footer from './components/Footer';
+import Home from './pages/index';
+import {Routes, Route} from 'react-router-dom';
+import About from './pages/about';
+import Menu from './pages/menu';
+import Contact from './pages/contact';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Hero />
-      <Content />
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="/menu" element={<Menu/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center h-16 bg-black text-white">
+        <p>Copyright © 2022 Rosa's Hamburger Stand All rights reserved. </p>
+    </div>
+  )
+}
+
+export default Footer

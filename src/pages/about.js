fc997cedf0e7f253206c6a7281e17abfe183159a
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-05-01 22:18:07 -0500

    photo captions

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,12 +8,17 @@ const About = () => {
     <div className="h-screen flex justify-center items-center bg-red-500">
         <h1 className="text-9xl uppercase font-black">How The Stand Got Started...</h1>
     </div>
-    <div className="h-screen flex justify-center items-center bg-black">
+
+
+    <div className="flex flex-col justify-center items-center bg-black h-screen font-mono py-40">
       <img src={Rosa} alt="rosa" className="h-full rounded mb-20 shadow object-scale-down"/>
-      <h2 className="mb-2 text-white">Rosa hard at work</h2>
+      <div className="flex justify-center items-center">
+        <h2 className="text-2xl mb-2 text-white">Rosa Hard At Work</h2>
+      </div>
     </div>
-    <div className="h-screen flex justify-center items-center bg-black">
+    <div className="flex flex-col justify-center items-center bg-black h-screen font-mono py-40">
       <img src={Family} alt="family" className="h-full rounded mb-20 shadow object-scale-down" />
+      <h2 className="text-2xl mb-2 text-white">Rosa and Daughters</h2>
     </div>
   </>
   )

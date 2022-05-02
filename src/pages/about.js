57bf4260a98c13f5d0425bbcf3307788a8cbfdc0
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-05-01 21:29:25 -0500

    images in about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Rosa from '../images/rosa.jpeg';
+import Family from '../images/rosafamily.jpeg';
 
 const About = () => {
   return (
-    <div className="h-screen flex justify-center items-center bg-yellow-300">
-        <h1 className="text-9xl uppercase font-black">About Page</h1>
+  <>
+    <div className="h-screen flex justify-center items-center bg-red-500">
+        <h1 className="text-9xl uppercase font-black">How The Stand Got Started...</h1>
     </div>
+    <div className="h-screen flex justify-center items-center bg-black">
+      <img src={Rosa} alt="rosa" className="h-full rounded mb-20 shadow object-scale-down"/>
+      <h2 className="mb-2 text-white">Rosa hard at work</h2>
+    </div>
+    <div className="h-screen flex justify-center items-center bg-black">
+      <img src={Family} alt="family" className="h-full rounded mb-20 shadow object-scale-down" />
+    </div>
+  </>
   )
 }
 
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Menupic from '../images/menu.jpeg';
 
 const Menu = () => {
   return (
+    <>
     <div className="h-screen flex justify-center items-center bg-blue-300">
-        <h1 className="text-9xl uppercase font-black">Menu Page</h1>
+        <h1 className="text-9xl uppercase font-black">Menu</h1>
     </div>
+    <div className="h-screen flex justify-center bg-black">
+      <img src={Menupic} alt="menu" className="h-full rounded mb-20 shadow" />
+    </div>
+    </>
   )
 }
 
